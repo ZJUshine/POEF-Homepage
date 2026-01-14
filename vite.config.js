09a9cfc1aee97a14fe52be7e65998196aaabd89a
ZJUshine <3190103641@zju.edu.cn>
2026-01-15 00:41:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/POEF-Homepage',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
